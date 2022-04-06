36331f8d8678575cd2402af31fb8b7f1b4d7147a
Thomas Lee <tfl@cookham.net>
2022-04-06 15:49:47 +0100

    Adding Goodies

--- Scripts/1.6 Installing-VSCode.ps1
+++ Scripts/1.6 Installing-VSCode.ps1
@@ -1,4 +1,4 @@
-﻿# 1.5 Install-VSCode
+﻿# 1.6 Install-VSCode
 #
 # Run on SRV1 after installing PowerShell 7
 # Run in PowerShell 7 console
@@ -8,7 +8,11 @@ $VSCPATH = 'C:\Foo'
 Save-Script -Name Install-VSCode -Path $VSCPATH
 Set-Location -Path $VSCPATH
 
-# 2. Running the installation script and adding in some popular extensions
+# 2. Reviewing the installation help details
+Get-Help -Name C:\Foo\Install-VSCode.ps1
+
+
+# 3. Running the installation script and adding in some popular extensions
 $Extensions =  'Streetsidesoftware.code-spell-checker',
                'yzhang.markdown-all-in-one',
                'hediet.vscode-drawio'
@@ -19,20 +23,21 @@ $InstallHT = @{
 }
 .\Install-VSCode.ps1 @InstallHT | Out-Null
 
-# 3. Exiting from VS Code
+# 4. Exiting from VS Code
 
-# 4. Restarting VS Code as an Administrator
+# 5. Restarting VS Code as an Administrator
 
-# 5. Opening a VS Code Terminal and running PowerShell 7.
+# 6. Opening a VS Code Terminal and running 
+#    PowerShell 7 as administrator
 
-# 6. Creating a profile file for VS Code
+# 7. Creating a profile file for VS Code
 $SAMPLE =
   'https://raw.githubusercontent.com/doctordns/PACKT-PS7/master/' +
   'scripts/goodies/Microsoft.VSCode_profile.ps1'
 (Invoke-WebRequest -Uri $Sample).Content |
   Out-File $Profile
 
-# 7. Updating local user settings for VS Code
+# 8. Updating local user settings for VS Code
 $JSON = @'
 {
   "workbench.colorTheme": "Visual Studio Light",
@@ -56,7 +61,7 @@ $JHT |
   ConvertTo-Json  |
     Out-File -FilePath $Settings
 
-# 8. Creating a short cut to VSCode
+# 9. Creating a short cut to VSCode
 $SourceFileLocation  = "$env:ProgramFiles\Microsoft VS Code\Code.exe"
 $ShortcutLocation    = "C:\foo\vscode.lnk"
 # Create a  new wscript.shell object
@@ -66,7 +71,7 @@ $Shortcut.TargetPath = $SourceFileLocation
 # Save the Shortcut to the TargetPath
 $Shortcut.Save()
 
-# 9. Creating a short cut to PowerShell 7
+# 10. Creating a short cut to PowerShell 7
 $SourceFileLocation  = "$env:ProgramFiles\PowerShell\7\pwsh.exe"
 $ShortcutLocation    = 'C:\Foo\pwsh.lnk'
 # Create a  new wscript.shell object
@@ -76,7 +81,7 @@ $Shortcut.TargetPath = $SourceFileLocation
 #Save the Shortcut to the TargetPath
 $Shortcut.Save()
 
-# 10. Building an updated Layout XML file
+# 11. Building an updated Layout XML file
 $XML = @'
 <?xml version="1.0" encoding="utf-8"?>
 <LayoutModificationTemplate
@@ -98,11 +103,11 @@ $XML = @'
 '@
 $XML | Out-File -FilePath C:\Foo\Layout.Xml
 
-# 11. Importing the  start layout XML file
+# 12. Importing the  start layout XML file
 #     You get an error if this is not run in an elevated session
 Import-StartLayout -LayoutPath C:\Foo\Layout.Xml -MountPath C:\
 
-# 12. Creating a profile file for PWSH 7 Consoles
+# 13. Creating a profile file for PWSH 7 Consoles
 $ProfileFolder = Join-Path ($Env:homeDrive+ $env:HOMEPATH) 'Documents\PowerShell'
 $ProfileFile2   = 'Microsoft.PowerShell_Profile.ps1'
 $ConsoleProfile = Join-Path -Path $ProfileFolder -ChildPath $ProfileFile2
@@ -113,7 +118,7 @@ $URI2 = 'https://raw.githubusercontent.com/doctordns/PACKT-PS7/master/' +
 (Invoke-WebRequest -Uri $URI2).Content |
   Out-File -FilePath  $ConsoleProfile
 
-# 13. Logging off 
+# 14. Logging off 
 logoff.exe
 
 # 14. Logging into Windows to observe the updated task bar
